c37080c0a74a2547ff2682845b5d25a8df0a05fd
machenmusik <machenmusik@gmail.com>
2017-09-08 11:47:20 -0400

    redo with internal cache in native form, and explicitly generating separate lists

--- src/three-ar-planes.js
+++ src/three-ar-planes.js
@@ -9,9 +9,14 @@ AFRAME.registerComponent('three-ar-planes', {
 
   tick: (function (t, dt) {
     // Create the temporary variables we will reuse, if needed.
+    var tempAlignment = 0;
     var tempScale = new THREE.Vector3(1, 1, 1);
-    var tempExtent = new THREE.Vector3();
+    var tempExtent3 = new THREE.Vector3();
     var tempMat4 = new THREE.Matrix4();
+    var tempPosition = new THREE.Vector3();
+    var tempRotation = new THREE.Vector3();
+    var tempQuaternion = new THREE.Quaternion();
+    var tempEuler = new THREE.Euler(0, 0, 0, 'YXZ');
 
     // The actual function, which we return.
     return function (t, dt) {
@@ -19,86 +24,119 @@ AFRAME.registerComponent('three-ar-planes', {
       var threear = this.el.components['three-ar'];
       if (!threear || !threear.arDisplay) { return; }
       var arDisplay = threear.arDisplay;
+
+      // Ideally we would have either events, or separate lists for added / updated / removed.
+      var addedThese = [];
+      var updatedThese = [];
+      var removedThese = [];
+
       // Because we don't have an indication of added / updated / removed,
       // try to keep track ourselves.
       var seenThese = {};
+      var i;
       // Iterate over the available planes.
       var planes = arDisplay.getPlanes ? arDisplay.getPlanes() : arDisplay.anchors_;
-      for (var i=0; planes && i<planes.length; i++) {
+      for (i=0; planes && i<planes.length; i++) {
         var plane = planes[i];
-
-        // Force plane conformance to common spec (almost latest spec).
-        if (!plane.identifier) { plane.identifier = plane.id; }
-        if (typeof plane.identifier !== 'string') {
-          plane.identifier = plane.identifier.toString();
+        // Get plane identifier.
+        // Force plane identifier conformance to common spec (almost latest spec).
+        var id = (plane.identifier !== undefined ? plane.identifier : plane.id).toString();
+        // Note that we've seen it.
+        seenThese[id] = true;
+        // Figure out whether added or updated.
+        var updatedThis = !this.planes.has(id);
+        if (this.planes.has(id)) {
+          // If we've seen it before, we should have it cached in this.planes already, to compare against.
+          if (!AFRAME.deepEqual(plane, this.planes[id])) {
+            updatedThese.push(plane);
+          }
+        } else {
+          // Remember by cloning the plane into this.planes.
+          this.planes[id] = JSON.parse(JSON.stringify(plane));
+          addedThese.push(plane);
         }
-        if (plane.polygon) { plane.vertices = plane.polygon; }     
-
-        seenThese[plane.identifier] = true;
-
-        // Unify position and orientation into vector3 and quaternion.
-        // Compute unification (since we have to assume it may be updated.)
-        // Note that although it is possible to construct the arrays for
-        // position and orientation from transform, it's not worth it,
-        // since we will want to use vector3 and quaternion anyway.
-        if (!plane.vector3) { plane.vector3 = new THREE.Vector3(); }
-        if (!plane.quaternion) { plane.quaternion = new THREE.Quaternion(); }
-        if (!plane.rotation) { plane.rotation = new THREE.Euler(0,0,0,'YXZ'); }
-        if (!plane.extent3) { plane.extent3 = new THREE.Vector3(); }
-        if (plane.transform) { // ARKit
-          tempMat4.fromArray(plane.transform);
-          tempMat4.decompose(plane.vector3, plane.quaternion, tempScale);
-        } else { // ARCore
-          plane.vector3.fromArray(plane.position);
-          plane.quaternion.fromArray(plane.orientation);
-        }
-        plane.rotation.setFromQuaternion(plane.quaternion);
-        if (plane.rotation.x) { plane.rotation.x *= THREE.Math.RAD2DEG; }
-        if (plane.rotation.y) { plane.rotation.y *= THREE.Math.RAD2DEG; }
-        if (plane.rotation.z) { plane.rotation.z *= THREE.Math.RAD2DEG; }
-        plane.extent3.set(plane.extent[0], 0, plane.extent[1]);
-
-        var planeExistsAlready = this.planes.has(plane.identifier);
-        var emitEvent = !planeExistsAlready 
-          // Is it cheaper to treat every time as an update, or to check?
-          // DEFINITELY cheaper to check.
-          // Unfortunately with current WebARonARKit, brute force is needed.
-          || !AFRAME.utils.deepEqual(plane, this.planes[plane.identifier]);
-        
-        if (emitEvent) {
-          // Remember the updated information. (Clone it.)
-          this.planes[plane.identifier] = JSON.parse(JSON.stringify(plane));
-
-          // Conversions to emit (legacy) event.
-          tempExtent.set(plane.extent[0], 0, plane.extent[1]);
-
-          // Emit event.
-          this.el.emit(planeExistsAlready ? 'updateplane' : 'createplane', {
-            id: plane.identifier,
-            alignment: plane.alignment || 0 /* horizontal */,
-            extent: plane.extent3,
-            position: plane.vector3,
-            rotation: plane.rotation,
-            scale: tempScale
-          });
-        }
-        
-        // TODO:
-        // If removed planes can still appear in the list,
-        // handle that here.
       }
-
-      // Iterate over planes we've seen; if they're removed, emit events.
-      var keys = Object.keys(this.planes);
-      for (var j=0; j<keys.length; j++) {
-        var key = keys[j];
+      // To find ones we've removed, we need to scan this.planes.
+      this.planes.forEach(function (key, value, map) {
         if (!seenThese[key]) {
-          // Emit event.
-          this.el.emit('removeplane', {id: key});
-          // Delete key.
-          this.planes.delete(key);
+          removedThese.push(value);
         }
-      }
+      });
+      // Remove any we should from the cache, afterward to avoid iteration races.
+      var self = this;
+      removedThese.forEach(function (plane) {
+        var id = (plane.identifier !== undefined ? plane.identifier : plane.id).toString();
+        self.planes.delete(id);
+      });
+
+      // OK, now we should have separate added / updated / removed lists,
+      // from which we can emit appropriate events downstream.
+
+      // First, emit remove events as appropriate.
+      removedThese.forEach(function (plane) {
+        var id = (plane.identifier !== undefined ? plane.identifier : plane.id).toString();
+        self.el.emit('removeplane', {id: id});
+      });
+
+      // Next, emit updated events as appropriate.
+      updatedThese.forEach(function (plane) {
+        var id = (plane.identifier !== undefined ? plane.identifier : plane.id).toString();
+	// Per spec, we get position (as array3), quaternion (as array4), and extent (as vec2).
+        // But ARKit doesn't follow spec, instead giving transform (mat4).
+	if (plane.transform) {
+          // ARKit
+          tempMat4.fromArray(plane.transform);
+          tempMat4.decompose(tempPosition, tempQuaternion, tempScale);
+        } else {
+          tempPosition.fromArray(plane.position);
+          tempQuaternion.fromArray(plane.orientation);
+	}
+        tempEuler.setFromQuaternion(tempQuaternion);
+        tempRotation.set(
+          tempEuler.x * THREE.Math.RAD2DEG,
+          tempEuler.y * THREE.Math.RAD2DEG,
+          tempEuler.z * THREE.Math.RAD2DEG);
+        tempExtent3.set(plane.extent[0], 0, plane.extent[1]);
+
+        self.el.emit('updateplane', {
+          id: id,
+          alignment: tempAlignment,
+          position: tempPosition,
+          rotation: tempRotation,
+          extent: tempExtent3,
+          vertices: plane.vertices || plane.polygon,
+          scale: tempScale});
+      });
+
+      // Last, emit added/created events as appropriate.
+      addedThese.forEach(function (plane) {
+        var id = (plane.identifier !== undefined ? plane.identifier : plane.id).toString();
+	// Per spec, we get position (as array3), quaternion (as array4), and extent (as vec2).
+        // But ARKit doesn't follow spec, instead giving transform (mat4).
+	if (plane.transform) {
+          // ARKit
+          tempMat4.fromArray(plane.transform);
+          tempMat4.decompose(tempPosition, tempQuaternion, tempScale);
+        } else {
+          tempPosition.fromArray(plane.position);
+          tempQuaternion.fromArray(plane.orientation);
+	}
+        tempEuler.setFromQuaternion(tempQuaternion);
+        tempRotation.set(
+          tempEuler.x * THREE.Math.RAD2DEG,
+          tempEuler.y * THREE.Math.RAD2DEG,
+          tempEuler.z * THREE.Math.RAD2DEG);
+        tempExtent3.set(plane.extent[0], 0, plane.extent[1]);
+
+        self.el.emit('createplane', {
+          id: id,
+          alignment: tempAlignment,
+          position: tempPosition,
+          rotation: tempRotation,
+          extent: tempExtent3,
+          vertices: plane.vertices || plane.polygon,
+          scale: tempScale});
+      });
     };    
   })()
 });
