abc43d3cabd715281f01ae4f59c7daebdbe54dec
M C <machenmusik@gmail.com>
2017-09-07 21:23:47 -0400

    update to accommodate both existing ARKit and draft ARCore

--- src/three-ar-planes.js
+++ src/three-ar-planes.js
@@ -9,74 +9,96 @@ AFRAME.registerComponent('three-ar-planes', {
 
   tick: (function (t, dt) {
     // Create the temporary variables we will reuse, if needed.
-    var tempPosition = new THREE.Vector3();
-    var tempQuaternion = new THREE.Quaternion();
-    var tempScale = new THREE.Vector3();
-    var tempRotation = new THREE.Euler(0, 0, 0, 'YXZ');
-    var tempMatrix4 = new THREE.Matrix4();
+    var tempScale = new THREE.Vector3(1, 1, 1);
     var tempExtent = new THREE.Vector3();
-    
+    var tempMat4 = new THREE.Matrix4();
+
     // The actual function, which we return.
     return function (t, dt) {
       // Check to see if the anchor list is available.
       var threear = this.el.components['three-ar'];
       if (!threear || !threear.arDisplay) { return; }
-      var anchors = threear.arDisplay.anchors_;
-      if (!anchors) { return; }
+      var arDisplay = threear.arDisplay;
+      // Because we don't have an indication of added / updated / removed,
+      // try to keep track ourselves.
+      var seenThese = {};
+      // Iterate over the available planes.
+      var planes = arDisplay.getPlanes ? arDisplay.getPlanes() : arDisplay.anchors_;
+      for (var i=0; planes && i<planes.length; i++) {
+        var plane = planes[i];
 
-      // Iterate over the available anchors.
-      for (var i=0; i<anchors.length; i++) {
-        var anchor = anchors[i];
-        tempMatrix4.elements = anchor.transform;
-        tempMatrix4.decompose(tempPosition, tempQuaternion, tempScale);
-        tempRotation.setFromQuaternion(tempQuaternion);
-        tempRotation.x *= THREE.Math.RAD2DEG;
-        tempRotation.y *= THREE.Math.RAD2DEG;
-        tempRotation.z *= THREE.Math.RAD2DEG;
-        tempExtent.set(anchor.extent[0], 0, anchor.extent[1]);
-        var planeExistsAlready = this.planes[anchor.identifier];
+        // Force plane conformance to common spec (almost latest spec).
+        if (!plane.identifier) { plane.identifier = plane.id; }
+        if (typeof plane.identifier !== 'string') {
+          plane.identifier = plane.identifier.toString();
+        }
+        if (plane.polygon) { plane.vertices = plane.polygon; }     
+
+        seenThese[plane.identifier] = true;
+
+        // Unify position and orientation into vector3 and quaternion.
+        // Compute unification (since we have to assume it may be updated.)
+        // Note that although it is possible to construct the arrays for
+        // position and orientation from transform, it's not worth it,
+        // since we will want to use vector3 and quaternion anyway.
+        if (!plane.vector3) { plane.vector3 = new THREE.Vector3(); }
+        if (!plane.quaternion) { plane.quaternion = new THREE.Quaternion(); }
+        if (!plane.rotation) { plane.rotation = new THREE.Euler(0,0,0,'YXZ'); }
+        if (!plane.extent3) { plane.extent3 = new THREE.Vector3(); }
+        if (plane.transform) { // ARKit
+          tempMat4.fromArray(plane.transform);
+          tempMat4.decompose(plane.vector3, plane.quaternion, tempScale);
+        } else { // ARCore
+          plane.vector3.fromArray(plane.position);
+          plane.quaternion.fromArray(plane.orientation);
+        }
+        plane.rotation.setFromQuaternion(plane.quaternion);
+        plane.rotation.x *= THREE.Math.RAD2DEG;
+        plane.rotation.y *= THREE.Math.RAD2DEG;
+        plane.rotation.z *= THREE.Math.RAD2DEG;
+        plane.extent3.set(plane.extent[0], 0, plane.extent[1]);
+
+        var planeExistsAlready = this.planes.has(plane.identifier);
         var emitEvent = !planeExistsAlready 
           // Is it cheaper to treat every time as an update, or to check?
           // DEFINITELY cheaper to check.
           // Unfortunately with current WebARonARKit, brute force is needed.
-          || !AFRAME.utils.deepEqual(anchor, this.planes[anchor.identifier]);
+          || !AFRAME.utils.deepEqual(plane, this.planes[plane.identifier]);
         
         if (emitEvent) {
-          // Remember the updated information.
-          this.planes[anchor.identifier] = JSON.parse(JSON.stringify(anchor));
-          this.planes[anchor.identifier].marked = true;
+          // Remember the updated information. (Clone it.)
+          this.planes[plane.identifier] = JSON.parse(JSON.stringify(plane));
+
+          // Conversions to emit (legacy) event.
+          tempExtent.set(plane.extent[0], 0, plane.extent[1]);
 
           // Emit event.
           this.el.emit(planeExistsAlready ? 'updateplane' : 'createplane', {
-            id: anchor.identifier,
-            alignment: anchor.alignment,
-            extent: tempExtent,
-            position: tempPosition,
-            rotation: tempRotation,
+            id: plane.identifier,
+            alignment: plane.alignment || 0 /* horizontal */,
+            extent: plane.extent3,
+            position: plane.vector3,
+            rotation: plane.rotation,
             scale: tempScale
           });
         }
         
         // TODO:
-        // If removed anchors can still appear in the list,
+        // If removed planes can still appear in the list,
         // handle that here.
       }
 
       // Iterate over planes we've seen; if they're removed, emit events.
-      var deleteThese = [];
-      this.planes.forEach(function(key, value, map) {
-        var plane = this.planes[key];
-        if (plane.marked) {
-          // Clear flag in preparation for next time.
-          plane.marked = false;
-        } else {
+      var keys = Object.keys(this.planes);
+      for (var j=0; j<keys.length; j++) {
+        var key = keys[j];
+        if (!seenThese[key]) {
           // Emit event.
-          this.el.emit('removeplane', {id: plane.identifier});
-          deleteThese.push(key);
+          this.el.emit('removeplane', {id: key});
+          // Delete key.
+          this.planes.delete(key);
         }
-      });
-      // Delete after we have looped over them in case there are races.
-      deleteThese.forEach(function(key) { delete this.planes[key]; });
+      }
     };    
   })()
 });
