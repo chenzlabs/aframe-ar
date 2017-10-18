7323b9720bb249f1cd1304bb4136fd0d4391a41f
M C <machenmusik@gmail.com>
2017-10-18 15:27:48 -0400

    bump dist

--- dist/aframe-three-ar.js
+++ dist/aframe-three-ar.js
@@ -412,6 +412,9 @@
 	  dependencies: ['three-ar-planes'],
 	  init: function () {
 	    this.el.sceneEl.setAttribute('vr-mode-ui', {enabled: false});
+	    // Ensure passthrough is visible, make sure A-Frame styles don't interfere.
+	    document.head.insertAdjacentHTML('beforeend', 
+	      '<style>html,body {background-color: transparent !important;}</style>');
 	  }
 	});
 
