0de5661e5e69acd49f259d0169e3b724dde7a3a7
M C <machenmusik@gmail.com>
2017-10-18 15:29:24 -0400

    make sure passthrough is visible (ARKit, FullScreenWebAR)

--- src/ar.js
+++ src/ar.js
@@ -2,5 +2,8 @@ AFRAME.registerComponent('ar', {
   dependencies: ['three-ar-planes'],
   init: function () {
     this.el.sceneEl.setAttribute('vr-mode-ui', {enabled: false});
+    // Ensure passthrough is visible, make sure A-Frame styles don't interfere.
+    document.head.insertAdjacentHTML('beforeend', 
+      '<style>html,body {background-color: transparent !important;}</style>');
   }
 });
