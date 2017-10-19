8ffef34430ffedb09c0c0daeb8d3b37a221edde7
M C <machenmusik@gmail.com>
2017-10-19 08:49:11 -0400

    apply parameters to three-ar

--- src/ar.js
+++ src/ar.js
@@ -1,6 +1,15 @@
 AFRAME.registerComponent('ar', {
+  schema: {
+    takeOverCamera: {default: true},
+    cameraUserHeight: {default: false}
+  },
   dependencies: ['three-ar-planes'],
   init: function () {
+    this.el.setAttribute('three-ar', {
+      takeOverCamera: this.data.takeOverCamera,
+      cameraUserHeight: this.data.cameraUserHeight
+    });
+
     this.el.sceneEl.setAttribute('vr-mode-ui', {enabled: false});
     // Ensure passthrough is visible, make sure A-Frame styles don't interfere.
     document.head.insertAdjacentHTML('beforeend', 
