e67a42b55240d09622e1533ad4ad82de75fd06d2
M C <machenmusik@gmail.com>
2017-12-20 19:09:02 -0800

    bump dist

--- dist/aframe-three-ar.js
+++ dist/aframe-three-ar.js
@@ -48,7 +48,7 @@
 	__webpack_require__(2);
 	__webpack_require__(3);
 	__webpack_require__(4);
-	__webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./ar-camera\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+	__webpack_require__(5);
 	__webpack_require__(6);
 
 
@@ -613,7 +613,68 @@
 
 
 /***/ }),
-/* 5 */,
+/* 5 */
+/***/ (function(module, exports) {
+
+	AFRAME.registerComponent('ar-camera', {
+	  schema: {
+	    enabled: {default:true}
+	  },
+
+	  init: function () {
+	    this.wasLookControlsEnabled = this.el.getAttribute('look-controls', 'enabled');
+	  },
+
+	  update: function (oldData) {
+	    if (!oldData || oldData.enabled !== this.data.enabled) {
+	      // Value changed, so react accordingly.
+	      if (this.data.enabled) {
+	        // Save camera look-controls enabled, and turn off for AR.
+	        this.wasLookControlsEnabled = this.el.getAttribute('look-controls', 'enabled');
+	        this.el.setAttribute('look-controls', 'enabled', false);
+	      } else {
+	        // Restore camera look-controls enabled.
+	        this.el.setAttribute('look-controls', 'enabled',
+	          this.wasLookControlsEnabled === true);
+	      }
+	    }
+	  },
+	  
+	  tick: function (t, dt) {
+	    if (!this.data.enabled) { return; }
+	    
+	    var whichar = this.checkWhichAR();
+	    if (!whichar) { return; }
+	    
+	    // Apply the pose position via setAttribute,
+	    // so that other A-Frame components can see the values.
+	    this.el.setAttribute('position', whichar.getPosition());
+
+	    // Apply the pose rotation via setAttribute,
+	    // so that other A-Frame components can see the values.
+	    this.el.setAttribute('rotation', whichar.getRotation());
+
+	    // Apply the projection matrix, if we're not in VR.
+	    if (!this.el.sceneEl.is('vr-mode')) {
+	      this.el.components.camera.projectionMatrix = whichar.getProjectionMatrix();
+	    }    
+	  },
+	  
+	  checkWhichAR: function () {
+	    if (!this.whichar) {
+	      var whichar = this.el.sceneEl.components['three-ar'];
+	      if (!whichar || !whichar.arDisplay) {
+	        whichar = this.el.sceneEl.components['mozilla-xr-ar'];
+	      }
+	      if (!whichar || !whichar.arDisplay) { return; }
+	      this.whichar = whichar;
+	    }
+	    return this.whichar;
+	  }  
+	});
+
+
+/***/ }),
 /* 6 */
 /***/ (function(module, exports) {
 
